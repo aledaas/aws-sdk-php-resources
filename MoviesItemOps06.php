9e1a7be452478882ea101853a40c6f03e2eebc6c
aledaas <aledaas@gmail.com>
2017-11-21 16:32:44 -0300

    Movies create load, queries more...

---
+++ MoviesItemOps06.php
@@ -0,0 +1,52 @@
+<?php
+require 'vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
+$sdk = new Aws\Sdk([
+    'endpoint'   => 'http://localhost:8000',
+    'region'   => 'us-west-2',
+    'version'  => 'latest'
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+$marshaler = new Marshaler();
+
+$tableName = 'Movies';
+
+$year = 2015;
+$title = 'The Big New Movie';
+
+$key = $marshaler->marshalJson('
+    {
+        "year": ' . $year . ', 
+        "title": "' . $title . '"
+    }
+');
+
+$eav = $marshaler->marshalJson('
+    {
+        ":val": 5 
+    }
+');
+
+$params = [
+    'TableName' => $tableName,
+    'Key' => $key,
+  //  'ConditionExpression' => 'info.rating <= :val',
+  //  'ExpressionAttributeValues'=> $eav
+];
+
+try {
+    $result = $dynamodb->deleteItem($params);
+    echo "Deleted item.\n";
+
+} catch (DynamoDbException $e) {
+    echo "Unable to delete item:\n";
+    echo $e->getMessage() . "\n";
+}
+
+?>
